54a978553efc8a2395120f470833a7a11586e62b
Sreeja <sreeja.in@gmail.com>
2017-08-01 14:57:57 +0200

    Added copyright notice

--- src/main/java/applications/Account.java
+++ src/main/java/applications/Account.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * ===========================================================
+ * This file is part of the CISE tool software.
+ *
+ * The CISE tool software contains proprietary and confidential information of Inria.
+ * All rights reserved. Reproduction, adaptation or distribution, in whole or in part, is
+ * forbidden except by express written permission of Inria.
+ * Version V1.5.1., July 2017
+ * Authors: Mahsa Najafzadeh, Michał Jabczyński, Marc Shapiro
+ * Copyright (C) 2017, Inria
+ * ===========================================================
+ ******************************************************************************/
+
 package applications;
 
 import z3fol.xpr.AnnotatedOperation;
@@ -12,13 +25,14 @@ import z3fol.xpr.AnnotatedOperation;
 @Op(Account.Debit.class)
 public class Account extends AnnotatedSchema {
 
-    @XPR(value ={"Int amount","Int balance" },type =XPR.Type.ARGUMENT)
+    @XPR(value ={"Int amount","Int balance", "Bool token"},type =XPR.Type.ARGUMENT)
     @XPR(value ="amount >= 0",type = XPR.Type.PRECONDITION)
     @XPR(value ="balance := balance + amount",type =XPR.Type.EFFECT)
     public static class Deposit extends AnnotatedOperation { }
 
-    @XPR(value ={"Int amount","Int balance"},type =XPR.Type.ARGUMENT)
-    @XPR(value ="amount >= 0",type = XPR.Type.PRECONDITION)
+    @XPR(value ={"Int amount","Int balance", "Bool token"},type =XPR.Type.ARGUMENT)
+    @XPR(value ={"amount >= 0", "amount<=balance"},type = XPR.Type.PRECONDITION)
     @XPR(value ="balance := balance - amount",type =XPR.Type.EFFECT)
+    @XPR(value ="token==true", type=XPR.Type.Token)
     public static class Debit extends AnnotatedOperation { }
 }
--- src/main/java/z3fol/model/State.java
+++ src/main/java/z3fol/model/State.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * ===========================================================
+ * This file is part of the CISE tool software.
+ *
+ * The CISE tool software contains proprietary and confidential information of Inria.
+ * All rights reserved. Reproduction, adaptation or distribution, in whole or in part, is
+ * forbidden except by express written permission of Inria.
+ * Version V1.5.1., July 2017
+ * Authors: Mahsa Najafzadeh, Michał Jabczyński, Marc Shapiro
+ * Copyright (C) 2017, Inria
+ * ===========================================================
+ ******************************************************************************/
+
 package z3fol.model;
 
 import com.microsoft.z3.BoolExpr;
